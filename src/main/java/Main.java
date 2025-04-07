442e0ca4d960e86be27ae2b55153d6bcddc8625b
Thomas Hoversen <thomas.ehov@gmail.com>
2025-04-06 19:44:30 -0700

    initial attempt at extracting URL path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,6 +12,7 @@ public class Main {
                       [ message-body ]
    */
   private static final String GOOD_RESPONSE = "HTTP/1.1 200 OK\r\n\r\n";
+  private static final String BAD_RESPONSE = "HTTP/1.1 404 Not Found\r\n\r\n";
 
   public static void main(String[] args) {
 
@@ -26,9 +27,39 @@ public class Main {
 
        Socket client = serverSocket.accept(); // Wait for connection from client.
        System.out.println("accepted new connection");
+
+       // todo get inputStream bytes, convert to string, then parse request:
+       // GET /index.html HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n
+       String message = client.getInputStream().readAllBytes().toString();
+       String[] parts = message.split("\r\n");
+       String requestLine = "";
+       String headers = "";
+       String body = "";
+       String urlPath = "";
+       if (parts.length == 0) {
+           client.getOutputStream().write(BAD_RESPONSE.getBytes());
+       } else {
+           requestLine = parts[0];
+       }
+       if (parts.length > 1) {
+           headers = parts[1];
+       }
+       if (parts.length > 2) {
+           body = parts[2];
+       }
+       if (!isGoodUrlPath(requestLine)) {
+           client.getOutputStream().write(BAD_RESPONSE.getBytes());
+       }
        client.getOutputStream().write(GOOD_RESPONSE.getBytes());
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
+  }
+
+  private static boolean isGoodUrlPath(String requestLine) {
+      String[] parts = requestLine.split(" ");
+      String url = parts[1];
+      String[] splitUrl = url.split("/");
+      return splitUrl.length <= 2;
   }
 }
